d94afa6c4ad9e2c5703ff790f43b21e01943e774
AVENKATASIVASANKAR <97689510+AVENKATASIVASANKAR@users.noreply.github.com>
2022-04-06 03:26:44 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -11,29 +11,57 @@ document.write('<br>');*/
 
 
 
-function bmi(weight,height){
-    var weight=prompt("enter your weight in kgs");
-    parseFloat(weight);
-    var height=prompt("enter your height in meters");
-    parseFloat(height);
-var Bmi=(weight/Math.pow(height,2));
-document.write(Bmi);
+function bmi(Weight,Height){
+    var Weight=document.getElementById("inp1").value;
+    parseFloat(Weight);
+    var Height=document.getElementById('inp2').value;
+    Height=Height/3.2808;
+    var BMI=Weight/(Height*Height);
+    document.write(BMI);
+    document.write('<br>')
 document.write('<br>')
-document.write('<br>')
-if (Bmi<=18.5){
+if (BMI<=18.5){
     document.write("<h1 style='color:red;'>your are under-weight</h1>")
 }
 else
-if(Bmi>=18.6 && Bmi<=24.5){
-    document.write("<h1 style='color:green;'>you are Healthy-Weight</h1>")
+if(BMI>=18.6 && BMI<=24.5){
+   document.write("<h1 style='color:green;'>you are Healthy-Weight</h1>")
 }
 else
-if(Bmi>=25.0 && Bmi <= 29.9){
-    document.write("<h1 style='color:orange;'>your are over-weight</h1>")
+if(BMI >=25.0 && BMI <= 29.9){
+    //document.write("<h1 style='color:orange;'>your are over-weight</h1>")
 }
 else
-if(Bmi>=30.0){
-    document.write("<h1 stle='color:purple;'>your are suffering with OBESITY</h1>")
+if(BMI >=30.0){
+   document.write("<h1 stle='color:purple;'>your are suffering with OBESITY</h1>")
 }
+
 }
 bmi()
+document.write("<br>");
+
+   //var weight=prompt("enter your weight in kgs");
+   //parseFloat(weight);
+   // var height=prompt("Enter your height in Meters");
+   // parseFloat(height);
+//var Bmi=(weight/Math.pow(height,2));
+//document.write(Bmi);
+/*document.write('<br>')
+document.write('<br>')
+if (BMI<=18.5){
+    document.write("<h1 style='color:red;'>your are under-weight</h1>")
+}
+else
+if(BMI>=18.6 && BMI<=24.5){
+   document.write("<h1 style='color:green;'>you are Healthy-Weight</h1>")
+}
+else
+if(BMI>=25.0 && BMI <= 29.9){
+    //document.write("<h1 style='color:orange;'>your are over-weight</h1>")
+}
+else
+if(BMI>=30.0){
+   document.write("<h1 stle='color:purple;'>your are suffering with OBESITY</h1>")
+}
+
+bmi()*/
